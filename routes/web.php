f317b11fd438cb71523c577912748fca975771fb
Edward Baitsewe <edward@monatemedia.com>
2024-03-22 02:49:56 +0200

    Generate Resource  Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
+
+Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+Route::post('/note', [NoteController::class, 'store'])->name('note.store');
